703a80ceb300d43621c6b8b445594d128002fd29
Bradley Evans <bradleyevans.x@gmail.com>
2024-10-13 21:49:34 -0400

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <main>
+      <NavBar></NavBar>
+      <Footer></Footer>
+    </main>
+  );
+}
+
+export default App;
